dfc2c8b0302856c4283d133287c339d7639ded4b
sofiarothman <swahlg@kth.se>
2020-12-28 09:34:53 +0100

    we are online!

--- src/main/java/org/lab2/App.java
+++ src/main/java/org/lab2/App.java
@@ -1,4 +1,9 @@
 package org.lab2;
+import com.mongodb.*;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
 
 /**
  * Hello world!
@@ -6,8 +11,24 @@ package org.lab2;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
+
         System.out.println( "Hello World!" );
+        // Creating a Mongo client
+        MongoClient mongo = MongoClients.create("mongodb://localhost:27017");
+
+        // Creating Credentials
+        MongoCredential credential;
+        credential = MongoCredential.createCredential("sampleUser", "Test",
+                "password".toCharArray());
+        System.out.println("Connected to the database successfully");
+
+        // Accessing the database
+        MongoDatabase database = mongo.getDatabase("Test");
+        System.out.println("Credentials ::"+ credential);
+
+        //Creating a collection
+        database.createCollection("BigMike");
+        System.out.println("Collection created successfully");
     }
 }
